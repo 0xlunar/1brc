9a0a4e294e81234e075f05ec8fd60a83af3a0a93
0xlunar <20531468+0xlunar@users.noreply.github.com>
2024-06-06 00:45:33 +1000

    1billion rows in 1m 30sec.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,33 @@
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::{BufRead, BufReader, Error, Read, Write};
+use std::fs::{File, OpenOptions};
+use std::io::{BufRead, BufReader, Error, Read, Seek, SeekFrom, Write};
 use std::slice::Chunks;
 use std::thread::JoinHandle;
 use std::time::{Duration, Instant};
 
-fn main() -> Result<(), std::io::Error> {
+fn main() -> Result<(), Error> {
     let now = Instant::now();
-    let (buffer, chunks) = read_file_into_chunks("./measurements.txt");
-    println!("Reading File Time: {}ms", now.elapsed().as_millis());
-    let now = Instant::now();
+    // // let (buffer, chunks) = read_file_into_chunks("./measurements.txt");
+    // let chunks = read_file_into_chunks_seek_read("./measurements.txt");
+    // println!("Reading File Time: {}ms", now.elapsed().as_millis());
+    // let now = Instant::now();
+    //
+    //
+    // // let workloads = chunks.into_iter().map(|(start, end)| {
+    // //     let buffer = buffer.get(start as usize..=end as usize).unwrap().to_vec();
+    // //     std::thread::spawn(move || {
+    // //         parse_chunk_bytes(buffer)
+    // //     })
+    // // }).collect::<Vec<JoinHandle<_>>>();
+    //
+    // let workloads = chunks.into_iter().map(|buffer| {
+    //     std::thread::spawn(move || {
+    //         parse_chunk_bytes(buffer)
+    //     })
+    // }).collect::<Vec<JoinHandle<_>>>();
+    //
 
-
-    let workloads = chunks.into_iter().map(|(start, end)| {
-        let buffer = buffer.get(start as usize..=end as usize).unwrap().to_vec();
-        std::thread::spawn(move || {
-            parse_chunk_bytes(buffer)
-        })
-    }).collect::<Vec<JoinHandle<_>>>();
+    let workloads = read_file_into_chunks_seek_read_parallel("./measurements.txt");
 
     let mut output = HashMap::new();
     for workload in workloads {
@@ -52,16 +62,83 @@ fn main() -> Result<(), std::io::Error> {
     Ok(())
 }
 
-fn read_file_into_lines(name: &str) -> Vec<String>{
+// https://stackoverflow.com/a/67128168
+fn seek_read<R>(reader: &mut R, offset: u64, buf: &mut [u8]) -> std::io::Result<()> where R: Read + Seek {
+    reader.seek(SeekFrom::Start(offset))?;
+    reader.read_exact(buf)?;
+    Ok(())
+}
+
+fn read_file_into_chunks_seek_read(name: &str) -> Vec<Vec<u8>>{
     let file = File::open(name).unwrap();
-    let reader = BufReader::new(file);
+    let file_size_bytes = file.metadata().unwrap().len();
+    println!("File Size: {:?}", file_size_bytes);
+    let thread_count = std::thread::available_parallelism().unwrap().get();
+    let mut reader = BufReader::new(file);
 
-    let lines = reader.lines().filter_map(|r| match r {
-        Ok(r) => Some(r),
-        Err(_) => None,
-    }).collect::<Vec<String>>();
+    let mut chunks = Vec::new();
+    let chunk_size = file_size_bytes / thread_count as u64;
+    println!("Chunk Size: {}", chunk_size);
 
-    lines
+    let mut start_byte = 0;
+    for _ in 0..thread_count {
+        let mut buffer = vec![0; chunk_size as usize];
+        seek_read(&mut reader, start_byte, &mut buffer).unwrap();
+        if buffer.len() == 0 {
+            break;
+        }
+        let end_byte = buffer.iter().enumerate().rfind(|(pos, byte)| {
+            **byte == b'\n'
+        }).unwrap().0 as u64;
+        buffer.truncate(end_byte as usize); // Shrink buffer to last new line.
+        let end_byte = if end_byte < file_size_bytes {
+            end_byte + 1
+        } else {
+            file_size_bytes
+        };
+        start_byte = (start_byte + end_byte).min(file_size_bytes);
+        chunks.push(buffer);
+    }
+
+    chunks
+}
+
+fn read_file_into_chunks_seek_read_parallel(name: &str) -> Vec<JoinHandle<HashMap<String,(i16,i16)>>>{
+    let file = File::open(name).unwrap();
+    let file_size_bytes = file.metadata().unwrap().len();
+    println!("File Size: {:?}", file_size_bytes);
+    let thread_count = std::thread::available_parallelism().unwrap().get();
+    let mut reader = BufReader::new(file);
+
+    let chunk_size = file_size_bytes / thread_count as u64;
+    println!("Chunk Size: {}", chunk_size);
+    let mut thread_pool = Vec::new();
+
+    let mut start_byte = 0;
+    for _ in 0..thread_count {
+        let mut buffer = vec![0; chunk_size as usize];
+        seek_read(&mut reader, start_byte, &mut buffer).unwrap();
+        if buffer.len() == 0 {
+            break;
+        }
+        let end_byte = buffer.iter().enumerate().rfind(|(pos, byte)| {
+            **byte == b'\n'
+        }).unwrap().0 as u64;
+        buffer.truncate(end_byte as usize); // Shrink buffer to last new line.
+        let thread = std::thread::spawn(move || {
+            parse_chunk_bytes(buffer)
+        });
+        thread_pool.push(thread);
+        let end_byte = if end_byte < file_size_bytes {
+            end_byte + 1
+        } else {
+            file_size_bytes
+        };
+        start_byte = (start_byte + end_byte).min(file_size_bytes);
+        // chunks.push(buffer);
+    }
+
+    thread_pool
 }
 
 fn read_file_into_chunks(name: &str) -> (Vec<u8>, Vec<(u64, u64)>){
@@ -86,9 +163,7 @@ fn read_file_into_chunks(name: &str) -> (Vec<u8>, Vec<(u64, u64)>){
         if buffer.len() == 0 {
             break;
         }
-        println!("Len: {:?}", buffer.len());
         let end_byte = buffer.find(|(pos, byte)| {
-            println!("Pos: {}, Byte: {}", pos, byte);
             **byte == b'\n'
         }).unwrap().0 as u64;
         let end_byte = if end_byte < file_size_bytes {
@@ -104,8 +179,6 @@ fn read_file_into_chunks(name: &str) -> (Vec<u8>, Vec<(u64, u64)>){
 }
 
 fn parse_chunk_bytes(buffer: Vec<u8>) -> HashMap<String, (i16, i16)> {
-    // println!("Start: {}, End: {}", start_byte, end_byte);
-    // let buffer: &[u8] = buffer.get(start_byte as usize..=end_byte as usize).unwrap();
     let mut results = HashMap::new();
     let mut station_full = false;
     let mut station = String::new();
@@ -116,27 +189,16 @@ fn parse_chunk_bytes(buffer: Vec<u8>) -> HashMap<String, (i16, i16)> {
     while let Some(byte) = buffer.next() {
         match byte {
             b';' => {
-                buffer.next();
                 station_full = true;
-                continue;
             },
             b'-' => {
                 sign = -1;
-                continue;
             },
             b'.' => {
-                let last_digit = buffer.next().unwrap();
-                let char = *last_digit as char;
-                if char.is_digit(10) {
-                    // println!("Char: {}", char);
-                    let last_digit = char.to_digit(10).unwrap() as i16;
-                    value = value * 10 + last_digit;
-                }
                 continue;
             },
             b'\n' => {
                 value = sign * value;
-                let station_clone = station.clone();
                 results.entry(station).and_modify(|(min, max)| {
                     if value < *min {
                         *min = value;
@@ -145,25 +207,17 @@ fn parse_chunk_bytes(buffer: Vec<u8>) -> HashMap<String, (i16, i16)> {
                     }
                 }).or_insert((value, value));
 
-                // println!("Station: {}, Value: {}", station_clone, value);
-
                 station_full = false;
                 station = String::new();
                 sign = 1;
                 value = 0;
-                continue;
             }
             _ => {
                 let char = *byte as char;
                 if !station_full {
                     station.push(char);
-                    continue;
-                } else {
-                    // println!("Char: {}", *byte as char);
-                    if char.is_digit(10) {
-                        value = value * 10 + (char.to_digit(10).unwrap() as i16);
-                    }
-                    continue;
+                } else if char.is_digit(10) {
+                    value = value * 10 + (char.to_digit(10).unwrap() as i16);
                 }
             }
         }
@@ -171,47 +225,3 @@ fn parse_chunk_bytes(buffer: Vec<u8>) -> HashMap<String, (i16, i16)> {
 
     results
 }
-
-
-fn get_chunk_size(lines: usize) -> usize {
-    let thread_count = std::thread::available_parallelism().unwrap().get();
-    lines / thread_count
-}
-
-fn parse_chunk(chunk: Vec<String>) -> Vec<(String, i16)> {
-    let mut output: Vec<(String, i16)> = Vec::new();
-    for data in chunk {
-        let (station, mut temperature) = data.split_once(";").unwrap();
-        let mut sign: i16 = 1;
-        let mut value: i16 = 0;
-
-        let mut chars = temperature.chars();
-        while let Some(char) = chars.next() {
-            if char.is_digit(10) {
-                value = value * 10 + (char.to_digit(10).unwrap() as i16);
-            } else if char == '-' {
-                sign = -1;
-            }
-        }
-        output.push((station.to_string(), value));
-    }
-
-    output
-}
-
-fn process_dataset(dataset: Vec<(String, i16)>) -> HashMap<String, (i16, i16)> {
-    let mut output: HashMap<String, (i16, i16)> = HashMap::new();
-
-    for (station, temperature) in dataset {
-        output.entry(station).and_modify(|(min, max)| {
-            if temperature < *min {
-                *min = temperature;
-            } else if temperature > *max {
-                *max = temperature;
-            }
-        }).or_insert((temperature, temperature));
-    }
-
-    output
-}
-
