cb5f05b85c9ae87fb1bd9966e96b48d16707d7ee
0xlunar <20531468+0xlunar@users.noreply.github.com>
2024-06-06 19:40:32 +1000

    added memmap version

--- src/mmap.rs
+++ src/mmap.rs
@@ -1,14 +1,26 @@
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{BufReader, Read, Seek, SeekFrom, Write};
+use std::io::Write;
+use std::sync::Arc;
 use std::thread::{JoinHandle, sleep};
 use std::time::{Duration, Instant};
+use memmap2::{Mmap, MmapOptions};
 
 fn main() {
     let start = Instant::now();
     let now = Instant::now();
 
-    let workloads = read_file_into_chunks("./measurements.txt");
+    let name = "./measurements.txt";
+    let file = File::open(name).unwrap();
+    let file_size_bytes = file.metadata().unwrap().len();
+    let thread_count = std::thread::available_parallelism().unwrap().get() as u64;
+    let mmap = unsafe {
+        MmapOptions::new().map(&file).unwrap()
+    };
+    let mmap_buffer = Arc::new(mmap);
+
+    // let workloads = read_file_into_chunks("./measurements.txt");
+    let workloads = read_file_into_chunks(mmap_buffer, thread_count, file_size_bytes);
     println!("Workload Finished: {}ms", now.elapsed().as_millis());
 
     let now = Instant::now();
@@ -47,41 +59,28 @@ fn main() {
     sleep(Duration::from_secs(30))
 }
 
-// https://stackoverflow.com/a/67128168
-fn seek_read<R>(reader: &mut R, offset: u64, buf: &mut [u8]) -> std::io::Result<()> where R: Read + Seek {
-    reader.seek(SeekFrom::Start(offset))?;
-    reader.read_exact(buf)?;
-    Ok(())
-}
-
-fn read_file_into_chunks(name: &str) -> Vec<JoinHandle<HashMap<String,(i16, i16)>>>{
-    let file = File::open(name).unwrap();
-    let file_size_bytes = file.metadata().unwrap().len();
-    let thread_count = std::thread::available_parallelism().unwrap().get();
-    let mut reader = BufReader::new(file);
-
+fn read_file_into_chunks(mmap: Arc<Mmap>, thread_count: u64, file_size_bytes: u64,) -> Vec<JoinHandle<HashMap<String,(i16, i16)>>>{
     let chunk_divisor = 4;
-    let chunk_size = file_size_bytes / thread_count as u64;
+    let chunk_size = file_size_bytes / thread_count;
     let small_chunk_size = chunk_size / chunk_divisor;
-    let mut thread_pool = Vec::new();
-
     println!("File Size: {} bytes\nSmall Chunk Size: {}\nThread Count: {}", file_size_bytes, small_chunk_size, thread_count);
 
+    let mut thread_pool = Vec::new();
     let mut start_byte = 0;
-    for _ in 0..(thread_count as u64 * chunk_divisor) {
-        let mut buffer = vec![0; small_chunk_size as usize];
-        seek_read(&mut reader, start_byte, &mut buffer).unwrap();
+    for _ in 0..(thread_count * chunk_divisor) {
+        // let start = Instant::now();
+        let end_byte = (small_chunk_size + start_byte) as usize;
+        let buffer = &mmap[start_byte as usize..end_byte];
         if buffer.len() == 0 {
             break;
         }
-
         let end_byte = buffer.iter().enumerate().rfind(|(_, byte)| {
             **byte == b'\n'
         }).unwrap().0 as u64;
 
-        buffer.truncate(end_byte as usize); // Shrink buffer to last new line.
+        let buffer = Arc::clone(&mmap);
         let thread = std::thread::spawn(move || {
-            parse_chunk_bytes(buffer)
+            parse_chunk_bytes(buffer, start_byte, start_byte + end_byte)
         });
         thread_pool.push(thread);
         let end_byte = if end_byte < file_size_bytes {
@@ -90,17 +89,19 @@ fn read_file_into_chunks(name: &str) -> Vec<JoinHandle<HashMap<String,(i16, i16)
             file_size_bytes
         };
         start_byte = (start_byte + end_byte).min(file_size_bytes);
+        // println!("Total time: {}ms", start.elapsed().as_millis());
     }
 
     thread_pool
 }
 
-fn parse_chunk_bytes(buffer: Vec<u8>) -> HashMap<String, (i16, i16)> {
+fn parse_chunk_bytes(buffer: Arc<Mmap>, start_byte: u64, end_byte: u64) -> HashMap<String, (i16, i16)> {
     let mut results = HashMap::new();
     let mut station_full = false;
     let mut station = String::new();
     let mut sign: i16 = 1;
     let mut value: i16 = 0;
+    let buffer = &buffer[start_byte as usize..end_byte as usize];
 
     let mut buffer = buffer.iter();
     while let Some(byte) = buffer.next() {
